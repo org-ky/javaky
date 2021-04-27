1d60c7be6896f118f8a57e016ca8c2ba7f1f421c
org-ky <kyko2323@gmail.com>
2021-04-27 21:35:08 +0200

    Delete EJBProject directory

--- EJBProject/ejbModule/com/orgky/examples/EJBProjectBeanLocal.java
+++
@@ -1,8 +0,0 @@
-package com.orgky.examples;
-
-import jakarta.ejb.Local;
-
-@Local
-public interface EJBProjectBeanLocal extends EJBProjectBeanIF {
-
-}
--- EJBProject/ejbModule/com/orgky/examples/EJBProjectBeanRemote.java
+++
@@ -1,9 +0,0 @@
-package com.orgky.examples;
-
-import jakarta.ejb.Remote;
-
-@Remote
-public interface EJBProjectBeanRemote extends EJBProjectBeanIF{
-
-	public void TestSessionBean(String str);
-}
